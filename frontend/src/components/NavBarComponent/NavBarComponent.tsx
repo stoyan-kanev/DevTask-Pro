f5cf13593d6e889d332eb55dd3617d7f8dad4a2d
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-07-04 22:30:05 +0300

    Added style to the navbar

--- frontend/src/components/NavBarComponent/NavBarComponent.tsx
+++ frontend/src/components/NavBarComponent/NavBarComponent.tsx
@@ -1,34 +1,26 @@
 import {useAuth} from "../../context/AuthContext.tsx";
 import {Link} from "react-router-dom";
-
+import './NavBarCoponent.css'
 
 export default function NavBarComponent() {
-
-    const { user ,logout,loading} = useAuth();
-
-
-
+    const { user, logout, loading } = useAuth();
 
     return (
-        <div>
-            <h1>DevTask</h1>
+        <div className="navbar">
+            <Link to="/" className="navbar-title">DevTask</Link>
 
-
-            {loading ? null : user ? (
-                <>
-                    <Link className="navbar-item" to="/" onClick={logout}>Logout</Link>
-                </>
-
-            ) : (
-                <>
-                    <Link className="navbar-item" to="/login">Login</Link>
-                    <Link className="navbar-item" to="/register">Register</Link>
-                </>
+            {!loading && (
+                <div className="navbar-links">
+                    {user ? (
+                        <Link className="navbar-item" to="/" onClick={logout}>Logout</Link>
+                    ) : (
+                        <>
+                            <Link className="navbar-item" to="/login">Login</Link>
+                            <Link className="navbar-item" to="/register">Register</Link>
+                        </>
+                    )}
+                </div>
             )}
-
-
         </div>
-    )
-
-
+    );
 }
