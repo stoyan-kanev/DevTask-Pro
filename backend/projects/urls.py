817bf36cbc91f8162fa943da7cfc67463aacd729
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-07-13 23:24:54 +0300

    Changed the project structure from React/Django to Angular/Node

--- backend/projects/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from projects.views import ProjectApiView
-
-urlpatterns = [
-    path('', ProjectApiView.as_view(), name='project-view'),
-]
--- backend/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from tasks.views import TaskApiView, TaskDetailApiView
-
-urlpatterns = [
-    path('', TaskApiView.as_view(), name='tasks'),
-    path('project/<uuid:project_id>/', TaskApiView.as_view()),
-    path('<int:pk>', TaskDetailApiView.as_view(), name='task-detail'),
-]
