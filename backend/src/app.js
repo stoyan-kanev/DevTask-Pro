c13ee691b8db524f34b67d97d19c4fc790f70ccb
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-07-14 20:07:35 +0300

    Started developing the api

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://admin:strongpassword@10.70.71.118:27017/dev_track_pro?authSource=admin')
+    .then(() => console.log("✅ MongoDB connected"))
+    .catch(err => console.error("❌ MongoDB connection error:", err));
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log("Server Listening on PORT:", port);
+});
+
+
+app.get('/', (req, res) => {
+    res.send("Hello from Express!")
+})
