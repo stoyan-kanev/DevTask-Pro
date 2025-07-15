1ad93ce011f4715a4e08496c45d16737f725ddc4
LazyPotato <74728512+LazyPotato02@users.noreply.github.com.>
2025-07-15 19:28:10 +0300

    Added login functionality to api prepping for JWT

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,22 +2,37 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const User = require('../models/User');
 
-exports.loginUser = (req, res) => {
-    // console.log(req.body)
+exports.loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        if (user) {
+            const isPasswordMatch = await bcrypt.compare(password, user.password);
+            if (isPasswordMatch) {
+                return res.status(200).json({message: "Logged in successfully"});
+            }
+            else{
+                res.status(401).json({message: 'Wrong password'})
+            }
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Failed to login', error: err});
+    }
 
 
-    res.json({
-        username: 'username'
-    });
 }
 
 exports.registerUser = async (req, res) => {
     try {
-        const { username, email, password } = req.body;
+        const {username, email, password} = req.body;
 
-        const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({email});
         if (existingUser) {
-            return res.status(409).json({ message: 'User already exists' });
+            return res.status(409).json({message: 'User already exists'});
         }
 
         const hash = await bcrypt.hash(password, saltRounds);
@@ -30,10 +45,10 @@ exports.registerUser = async (req, res) => {
 
         await user.save();
 
-        res.status(201).json({ username, email });
+        res.status(201).json({username, email});
     } catch (err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error', error: err.message });
+        res.status(500).json({message: 'Server error', error: err.message});
     }
 }
 
